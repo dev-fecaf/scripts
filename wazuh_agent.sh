02e57efe8aed2f3cfd0965bb1ede0226c76188b1
Victor Hermon <victorsilva.silva6@gmail.com>
2025-06-15 12:56:54 -0300

    Update wazuh_agent.sh

--- wazuh_agent.sh
+++ wazuh_agent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.7.2-1_amd64.deb && \
-sudo WAZUH_MANAGER='wazuh-unifecaf.ddns.net' dpkg -i ./wazuh-agent_4.7.2-1_amd64.deb && \
+sudo WAZUH_MANAGER='wazuh.unifecaf.edu.br' dpkg -i ./wazuh-agent_4.7.2-1_amd64.deb && \
 sudo systemctl daemon-reload && \
 sudo systemctl enable wazuh-agent && \
 sudo systemctl start wazuh-agent
