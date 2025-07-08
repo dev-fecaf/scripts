a0731f0f76a6c34b2841b84f8a0d7d64a17b7ce4
Victor Hermon <victorsilva.silva6@gmail.com>
2025-07-08 14:00:39 -0300

    Update find_container.sh

--- find_container.sh
+++ find_container.sh
@@ -1,9 +1,2 @@
-#!/usr/bin/env bash
-if [ -z "$1" ]; then
-  echo "Uso: $0 <PID>"
-  exit 1
-fi
-
-PID=$1
-CID=$(grep -oE '[0-9a-f]{64}' /proc/$PID/cgroup | head -n1)
-docker ps --no-trunc | grep $CID | awk '{print $1}'
+CID=$(grep -oE -e '[0-9a-f]{64}' /proc/$1/cgroup | head -n1)
+docker ps --no-trunc | grep "$CID" | awk '{print $1}'
